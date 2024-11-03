0a9423b0e72bb288ecefbefe6b695032db5eb0cb
Day225rd <kylejackson2256@gmail.com>
2024-11-03 00:46:18 -0400

    Changed database to work with new ideas
    Instead of the approach i was taking, gonna make the process easier by storing the questions in the database and having the code pull the questions directly from the DB rather than what i was doing which would've taken way too much work. By the end the program should just be able to pull everything it needs directly from the DB rather than relying so much on the code.

--- testing.py
+++ testing.py
@@ -49,3 +49,105 @@
 
         # self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
         # self.radiobutton.place(x=200,y=120)
+
+# cursor.execute('''
+#                CREATE TABLE IF NOT EXISTS Course2
+               
+#                (
+#                questions1 TEXT PRIMARY KEY
+#                , 
+#                questions2 TEXT
+#                ,
+#                questions3 TEXT
+#                ,
+#                questions4 TEXT
+#                ,
+#                questions5 TEXT
+#                ,
+#                questions6 TEXT
+#                , 
+#                questions7 TEXT
+#                ,
+#                questions8 TEXT
+#                ,
+#                questions9 TEXT
+#                ,
+#                questions10 TEXT
+#                               )
+#                ''')
+# cursor.execute('''
+#                CREATE TABLE IF NOT EXISTS Course3
+               
+#                (
+#                questions1 TEXT PRIMARY KEY
+#                , 
+#                questions2 TEXT
+#                ,
+#                questions3 TEXT
+#                ,
+#                questions4 TEXT
+#                ,
+#                questions5 TEXT
+#                ,
+#                questions6 TEXT
+#                , 
+#                questions7 TEXT
+#                ,
+#                questions8 TEXT
+#                ,
+#                questions9 TEXT
+#                ,
+#                questions10 TEXT
+#                               )
+#                ''')
+# cursor.execute('''
+#                CREATE TABLE IF NOT EXISTS Course4
+               
+#                (
+#                questions1 TEXT PRIMARY KEY
+#                , 
+#                questions2 TEXT
+#                ,
+#                questions3 TEXT
+#                ,
+#                questions4 TEXT
+#                ,
+#                questions5 TEXT
+#                ,
+#                questions6 TEXT
+#                , 
+#                questions7 TEXT
+#                ,
+#                questions8 TEXT
+#                ,
+#                questions9 TEXT
+#                ,
+#                questions10 TEXT
+#                               )
+#                ''')
+# cursor.execute('''
+#                CREATE TABLE IF NOT EXISTS Course5
+               
+#                (
+#                questions1 TEXT PRIMARY KEY
+#                , 
+#                questions2 TEXT
+#                ,
+#                questions3 TEXT
+#                ,
+#                questions4 TEXT
+#                ,
+#                questions5 TEXT
+#                ,
+#                questions6 TEXT
+#                , 
+#                questions7 TEXT
+#                ,
+#                questions8 TEXT
+#                ,
+#                questions9 TEXT
+#                ,
+#                questions10 TEXT
+#                               )
+#                ''')
+
