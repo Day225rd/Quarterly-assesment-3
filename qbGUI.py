1d57d174bd8ef9a7cd9fcf449211245452f139a3
Day225rd <kylejackson2256@gmail.com>
2024-11-03 16:16:47 -0500

    Trying to connect buttons with database,
    No idea how to do this

--- qbGUI.py
+++ qbGUI.py
@@ -8,7 +8,8 @@ import sqlite3
 
 
 #database connecction
-con = sqlite3.connect('quizBowldb.db')
+
+con = sqlite3.connect('quizBowlDB2.db')
 cur = con.cursor()
 
 #class for buttons and text box
@@ -17,9 +18,14 @@ cur = con.cursor()
 root =tk.Tk()
 root.title("ULTIMATE QUIZBOWL")
 
+
 class QuizBowlApp:
     def __init__(self,master):
         root.geometry("450x450")
+
+        self.m = master
+
+
 
         self.label = ttk.Label(master,text = "Welcome to the quizbowl game!")
         self.label.place(x=100,y=50)
@@ -45,22 +51,48 @@ class QuizBowlApp:
         self.radiobutton = ttk.Radiobutton(root, text = "Random", variable = click, value = "Elden Ring")
         self.radiobutton.place(x=180,y=140)  
 
-        self.btn1 = ttk.Button(root, text = "Press to start!")
+        self.btn1 = ttk.Button(root, text = "Press to start!", command = self.opennew_window)
         self.btn1.place (x=180,y=240)
 
-    def selection(self):
-        cur = self.conn.cur()
-        cur.execute('''SELECT DISTINCT course FROM question''')
-        courses = [row[0] for row in cur.fetchall()]
-        return courses
-    
-    # def start(self):
+    def obtaincourse(self):
+            cur = self.conn.cursor()
+            cur.execute('''SELECT DISTINCT course FROM Course''')
+            courses = [row[0] for row in cur.fetchall()]
+            return courses
         
+    def opennew_window(self):
+        new_window(self.m)
 
-# class QuizBowlquestionWindow:
-#     def __init__(self,master):
+class new_window:
+    def __init__(self,master):
+        root.geometry("450x450")
 
+
+        self.newwindow = tk.Toplevel(master)
+        self.newwindow.title("Questions")
+
+        self.label1 = tk.Label(self.newwindow,text = "Please enter the correct answer below")
+        self.label1.place(x = 30, y = 40)
+
+        self.label1 = ttk.Label(self.newwindow,text = "Enter text here:")
+        self.label1.place(x = 30, y = 60)
+
+        self.txtfield1 = ttk.Entry(self.newwindow)
+        self.txtfield1.place(x = 30, y = 80)
+
+        self.btn1 = ttk.Button(self.newwindow, text = "Submit Answer")
+        self.btn1.place(x = 30, y = 100)
+
+     
+
+
+    def retreivequestion(self):
+        root.geometry("450x450")
+        cur = self.conn.cursor()
+        question = self.mathquestion.get()
+        fetch = cur.execute('''SELECT * FROM Course WHERE course = ?''', (question))
+        self.label1 = tk.Label(self.newwindow, text = fetch)
+        self.label1.place(x = 30, y = 20)
 
 qBapp = QuizBowlApp(root)
 root.mainloop()
-
