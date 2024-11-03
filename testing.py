b5b112a08370c0109749f0a4e32ae6e00fc7bce7
Day225rd <kylejackson2256@gmail.com>
2024-11-02 22:44:16 -0400

    Minor changes

--- testing.py
+++ testing.py
@@ -18,16 +18,16 @@
 
 # root.mainloop()
 
-    def show():
-        label1 = Label( text = clicked1.get())
+    # def show():
+    #     label1 = Label( text = clicked1.get())
 
-        options1 = [
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"
-        ]
+    #     options1 = [
+    #         "1",
+    #         "2",
+    #         "3",
+    #         "4",
+    #         "5"
+    #     ]
 
     #drop down menu for courses
         # questions = {"1","2","3","4","5"}
@@ -35,17 +35,17 @@
         # self.dropdown_menu = ttk.OptionMenu(root, click, *questions)
         # self.dropdown_menu.place(x=200,y=200)   
 
-        self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
-        self.radiobutton.place(x=200,y=200)
+        # self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
+        # self.radiobutton.place(x=200,y=200)
 
-        self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
-        self.radiobutton.place(x=200,y=180)
+        # self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
+        # self.radiobutton.place(x=200,y=180)
 
-        self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
-        self.radiobutton.place(x=200,y=160)
+        # self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
+        # self.radiobutton.place(x=200,y=160)
 
-        self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
-        self.radiobutton.place(x=200,y=140)
+        # self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
+        # self.radiobutton.place(x=200,y=140)
 
-        self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
-        self.radiobutton.place(x=200,y=120)
+        # self.radiobutton = ttk.Radiobutton(root, text = click, value = "")
+        # self.radiobutton.place(x=200,y=120)
